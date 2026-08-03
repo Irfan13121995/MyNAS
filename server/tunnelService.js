d740d5064e6e73f943416305866a1efefc018142
Irfan13121995 <53309799+Irfan13121995@users.noreply.github.com>
2026-08-03 14:01:49 +0200

    feat(tunnel): Set mynas-hi.eu.org as target domain for Permanent Named Tunnel

--- server/tunnelService.js
+++ server/tunnelService.js
@@ -24,7 +24,7 @@ function getNamedTunnelConfig() {
   } catch (err) {
     console.warn('[Tunnel] Error reading tunnel config:', err.message);
   }
-  return { mode: 'quick', token: '', customUrl: 'https://mynas.eu.org' };
+  return { mode: 'quick', token: '', customUrl: 'https://mynas-hi.eu.org' };
 }
 
 function saveNamedTunnelConfig(config) {
